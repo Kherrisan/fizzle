5ad32b0fee9919f5543da0a1b29dbb753b628470
Nathaniel <me@nathanielbennett.com>
2024-05-23 09:31:05 -0400

    Revise IoLocation interface

--- fizzle-common/src/io.rs
+++ fizzle-common/src/io.rs
@@ -1,6 +1,16 @@
+
+// `SocketAddr` does not use heap allocations, so it's safe for this type.
+use std::os::unix::net::SocketAddr;
+
+use crate::path::FilePath;
+
 pub enum IoLocation {
-    File,
-    TcpSocket,
-    UdpSocket,
-    SctpSocket,
+    /// Treats `stdin`/`stdout` as an I/O location.
+    /// 
+    /// `stderr` is currently reserved for error messaging by fizzle.
+    Stdio,
+    File(FilePath),
+    TcpSocket(SocketAddr),
+    UdpSocket(SocketAddr),
+    SctpSocket(SocketAddr),
 }
