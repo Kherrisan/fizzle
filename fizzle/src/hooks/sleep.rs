9c938c2669da02afb24ab915ae5e63bcd24ac594
Nathaniel <me@nathanielbennett.com>
2024-06-06 09:41:02 -0400

    Handle long sleeps

--- fizzle/src/hooks/sleep.rs
+++ fizzle/src/hooks/sleep.rs
@@ -2,18 +2,20 @@ use crate::hook_macros;
 
 hook_macros::hook! {
     unsafe fn sleep(
-        _seconds: libc::c_uint
-    ) -> libc::c_uint => fizzle_sleep(_ctx) {
+        seconds: libc::c_uint
+    ) -> libc::c_uint => fizzle_sleep(ctx) {
         // TODO: how do we handle timeouts in the general case?
+        ctx.yield_thread();
         return 0
     }
 }
 
 hook_macros::hook! {
     unsafe fn usleep(
-        _usec: libc::useconds_t
-    ) -> libc::c_int => fizzle_usleep(_ctx) {
+        usec: libc::useconds_t
+    ) -> libc::c_int => fizzle_usleep(ctx) {
         // TODO: how do we handle timeouts in the general case?
+        ctx.yield_thread();
         return 0
     }
 }
