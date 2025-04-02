c313baa481a197719a0ebcd911c6a54aeb9a7a5a
Nathaniel Bennett <me@nathanielbennett.com>
2025-04-02 12:36:42 -0400

    Change uid/gid to 1001

--- fizzle/src/hooks/uid.rs
+++ fizzle/src/hooks/uid.rs
@@ -4,8 +4,8 @@ hook_macros::hook! {
     unsafe fn getuid() -> libc::uid_t => fizzle_getuid(ctx) {
         crate::strace!("getuid() -> ...");
 
-        crate::strace!("getuid() -> 1000");
-        1000
+        crate::strace!("getuid() -> 1001");
+        1001
     }
 }
 
@@ -13,8 +13,8 @@ hook_macros::hook! {
     unsafe fn geteuid() -> libc::uid_t => fizzle_geteuid(ctx) {
         crate::strace!("geteuid() -> ...");
 
-        crate::strace!("geteuid() -> 1000");
-        1000
+        crate::strace!("geteuid() -> 1001");
+        1001
     }
 }
 
@@ -22,8 +22,8 @@ hook_macros::hook! {
     unsafe fn getgid() -> libc::uid_t => fizzle_getgid(ctx) {
         crate::strace!("getgid() -> ...");
 
-        crate::strace!("getgid() -> 1000");
-        1000
+        crate::strace!("getgid() -> 1001");
+        1001
     }
 }
 
@@ -31,7 +31,7 @@ hook_macros::hook! {
     unsafe fn getegid() -> libc::uid_t => fizzle_getegid(ctx) {
         crate::strace!("getegid() -> ...");
 
-        crate::strace!("getegid() -> 1000");
-        1000
+        crate::strace!("getegid() -> 1001");
+        1001
     }
 }
