839462c8a5c46b3fa2aa395c311cb7db98f20dfe
Nathaniel Bennett <me@nathanielbennett.com>
2025-04-19 09:51:26 -0400

    Add `res_querydomain()`, `res_nquerydomain()`

--- fizzle/src/hooks/resolv.rs
+++ fizzle/src/hooks/resolv.rs
@@ -1,9 +1,9 @@
+use std::ffi::CStr;
 use std::{cmp, ptr, slice};
 
 use crate::hook_macros;
 use crate::scheduler::Scheduler;
 use crate::handlers::resolv::*;
-
 
 hook_macros::hook! {
     unsafe fn res_query(
@@ -70,6 +70,99 @@ hook_macros::hook! {
             }
             Err(e) => {
                 crate::strace!("res_nquery(statep={:?}, dname={:?}, class={}, ty={}, answer={:?}, anslen={}) -> -1", statep, dname, class, ty, answer, anslen);
+                -1
+            }
+        }
+    }
+}
+
+hook_macros::hook! {
+    unsafe fn res_querydomain(
+        name: *const libc::c_char,
+        domain: *const libc::c_char,
+        class: libc::c_int,
+        ty: libc::c_int,
+        answer: *mut libc::c_char,
+        anslen: libc::c_int
+    ) -> libc::c_int => fizzle_res_querydomain(ctx) {
+        let mut buf = [0u8; 1024];
+
+        let name_cstr = CStr::from_ptr(name);
+        let domain_cstr = CStr::from_ptr(domain);
+
+        crate::strace!("res_querydomain(name={:?}, domain={:?}, class={}, ty={}, answer={:?}, anslen={}) -> ...", name_cstr, domain_cstr, class, ty, answer, anslen);
+
+        let mut dname_vec = Vec::new();
+        dname_vec.extend_from_slice(name_cstr.to_bytes());
+        dname_vec.push(b'.');
+        dname_vec.extend_from_slice(domain_cstr.to_bytes());
+        dname_vec.push(b'\0');
+        let dname = dname_vec.as_ptr().cast();
+
+        let len = crate::res_mkquery(0, dname, class, ty, ptr::null_mut(), 0, ptr::null_mut(), buf.as_mut_ptr(), 1024);
+        if len < 0 {
+            log::error!("res_mkquery() failed for res_querydomain");
+            crate::strace!("res_querydomain(name={:?}, domain={:?}, class={}, ty={}, answer={:?}, anslen={}) -> -1", name_cstr, domain_cstr, class, ty, answer, anslen);
+            return -1
+        }
+
+        match Scheduler::handle_event(&mut ctx, DnsResolveEvent::new(&buf[..len as usize])) {
+            Ok(response) => {
+                let len = cmp::min(response.len(), anslen as usize);
+                let answer_slice = slice::from_raw_parts_mut(answer.cast::<u8>(), anslen as usize);
+                answer_slice[..len].copy_from_slice(&response[..len]);
+                crate::strace!("res_querydomain(name={:?}, domain={:?}, class={}, ty={}, answer={:?}, anslen={}) -> {}", name, domain, class, ty, answer, anslen, len);
+                len as libc::c_int // TODO: correct behavior on truncation?
+            }
+            Err(e) => {
+                crate::strace!("res_querydomain(name={:?}, domain={:?}, class={}, ty={}, answer={:?}, anslen={}) -> -1", name, domain, class, ty, answer, anslen);
+                -1
+            }
+        }
+    }
+}
+
+hook_macros::hook! {
+    unsafe fn res_nquerydomain(
+        statep: *mut libc::c_void,
+        name: *const libc::c_char,
+        domain: *const libc::c_char,
+        class: libc::c_int,
+        ty: libc::c_int,
+        answer: *mut libc::c_char,
+        anslen: libc::c_int
+    ) -> libc::c_int => fizzle_res_nquerydomain(ctx) {
+        let mut buf = [0u8; 1024];
+
+        let name_cstr = CStr::from_ptr(name);
+        let domain_cstr = CStr::from_ptr(domain);
+
+        crate::strace!("res_nquerydomain(statep={:?}, name={:?}, domain={:?}, class={}, ty={}, answer={:?}, anslen={}) -> ...", statep, name_cstr, domain_cstr, class, ty, answer, anslen);
+
+        let mut dname_vec = Vec::new();
+        dname_vec.extend_from_slice(name_cstr.to_bytes());
+        dname_vec.push(b'.');
+        dname_vec.extend_from_slice(domain_cstr.to_bytes());
+        dname_vec.push(b'\0');
+        let dname = dname_vec.as_ptr().cast();
+
+        let len = crate::res_mkquery(0, dname, class, ty, ptr::null_mut(), 0, ptr::null_mut(), buf.as_mut_ptr(), 1024);
+        if len < 0 {
+            log::error!("res_mkquery() failed for res_nquerydomain");
+            crate::strace!("res_nquerydomain(statep={:?}, name={:?}, domain={:?}, class={}, ty={}, answer={:?}, anslen={}) -> -1", statep, name_cstr, domain_cstr, class, ty, answer, anslen);
+            return -1
+        }
+
+        match Scheduler::handle_event(&mut ctx, DnsResolveEvent::new(&buf[..len as usize])) {
+            Ok(response) => {
+                let len = cmp::min(response.len(), anslen as usize);
+                let answer_slice = slice::from_raw_parts_mut(answer.cast::<u8>(), anslen as usize);
+                answer_slice[..len].copy_from_slice(&response[..len]);
+                crate::strace!("res_nquerydomain(statep={:?}, name={:?}, domain={:?}, class={}, ty={}, answer={:?}, anslen={}) -> {}", statep, name, domain, class, ty, answer, anslen, len);
+                len as libc::c_int // TODO: correct behavior on truncation?
+            }
+            Err(e) => {
+                crate::strace!("res_nquerydomain(statep={:?}, name={:?}, domain={:?}, class={}, ty={}, answer={:?}, anslen={}) -> -1", statep, name, domain, class, ty, answer, anslen);
                 -1
             }
         }
